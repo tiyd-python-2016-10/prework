ae2f04a7c2ec9fe2d6495d899dff10d8cde7fe3c
Sam Boyarsky <sam.boyarsky@theironyard.com>
2016-10-31 10:10:04 -0400

    week 2 begins

---
+++ week2/example.py
@@ -0,0 +1,17 @@
+
+kansas_pi = 3
+
+
+
+def say_hi_mom():
+    print("Yo, wazzup, Mom")
+
+
+def main():
+    print("charlie brown's teacher says: wah wah wah wah wah wah wah")
+
+if __name__ == "__main__":
+    print(__name__)
+    print("Hi mom")
+
+
